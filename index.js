8c17ffd09143c820fe6c9d549183811e76a30af5
Nick Barry <itsnickbarry@protonmail.ch>
2022-01-06 00:26:17 -0600

    simplify config setup

--- index.js
+++ index.js
@@ -4,28 +4,20 @@ require('./tasks/clear_abi.js');
 require('./tasks/export_abi.js');
 require('./tasks/compile.js');
 
-function withDefaults(abiExporterConfig) {
-  return Object.assign(
-    {
-      path: './abi',
-      runOnCompile: false,
-      clear: false,
-      flat: false,
-      only: [],
-      except: [],
-      spacing: 2,
-      pretty: false,
-      filter: () => true,
-    },
-    abiExporterConfig
-  );
-}
+const DEFAULT_CONFIG = {
+  path: './abi',
+  runOnCompile: false,
+  clear: false,
+  flat: false,
+  only: [],
+  except: [],
+  spacing: 2,
+  pretty: false,
+  filter: () => true,
+};
 
 extendConfig(function (config, userConfig) {
-  let abiExporterConfig = userConfig.abiExporter;
-  if (!Array.isArray(abiExporterConfig)) {
-    abiExporterConfig = [abiExporterConfig];
-  }
-
-  config.abiExporter = abiExporterConfig.map(withDefaults);
+  config.abiExporter = [userConfig.abiExporter].flatten().map(function (el) {
+    return Object.assign(DEFAULT_CONFIG, el);
+  });
 });
