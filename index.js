389d1b8df1c09b480ca1c04f4cbd029fbbf1bc24
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-02-02 12:53:52 -0700

    feat: Validate config in extendConfig (#36)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { extendConfig } = require('hardhat/config');
+const { HardhatPluginError } = require('hardhat/plugins');
+const { name: PLUGIN_NAME } = require('./package.json')
 
 require('./tasks/clear_abi.js');
 require('./tasks/export_abi.js');
@@ -16,8 +18,30 @@ const DEFAULT_CONFIG = {
   filter: () => true,
 };
 
+function validate(config, key, type) {
+  if (type === 'array') {
+    if (!Array.isArray(config[key])) {
+      throw new HardhatPluginError(PLUGIN_NAME, `\`${key}\` config must be an ${type}`)
+    }
+  } else {
+    if (typeof config[key] !== type) {
+      throw new HardhatPluginError(PLUGIN_NAME, `\`${key}\` config must be a ${type}`);
+    }
+  }
+}
+
 extendConfig(function (config, userConfig) {
   config.abiExporter = [userConfig.abiExporter].flat().map(function (el) {
-    return Object.assign({}, DEFAULT_CONFIG, el);
+    const conf = Object.assign({}, DEFAULT_CONFIG, el);
+    validate(conf, 'path', 'string');
+    validate(conf, 'runOnCompile', 'boolean');
+    validate(conf, 'clear', 'boolean');
+    validate(conf, 'flat', 'boolean');
+    validate(conf, 'only', 'array');
+    validate(conf, 'except', 'array');
+    validate(conf, 'spacing', 'number');
+    validate(conf, 'pretty', 'boolean');
+    validate(conf, 'filter', 'function');
+    return conf;
   });
 });
