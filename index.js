2653d244afeef70f3d861c3ea71e48054e9eff71
Nick Barry <itsnickbarry@protonmail.ch>
2022-04-27 23:24:19 -0500

    add missing path import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const { extendConfig } = require('hardhat/config');
 const { HardhatPluginError } = require('hardhat/plugins');
-const { name: PLUGIN_NAME } = require('./package.json')
+const { name: PLUGIN_NAME } = require('./package.json');
 
 require('./tasks/clear_abi.js');
 require('./tasks/export_abi.js');
@@ -21,7 +22,7 @@ const DEFAULT_CONFIG = {
 function validate(config, key, type) {
   if (type === 'array') {
     if (!Array.isArray(config[key])) {
-      throw new HardhatPluginError(PLUGIN_NAME, `\`${key}\` config must be an ${type}`)
+      throw new HardhatPluginError(PLUGIN_NAME, `\`${key}\` config must be an ${type}`);
     }
   } else {
     if (typeof config[key] !== type) {
