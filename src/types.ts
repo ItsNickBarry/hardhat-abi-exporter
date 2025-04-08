b057b72946d8d403af9c206af78ac0b53cfd1f1f
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-08 15:48:03 -0600

    define AbiExporterUserConfigEntry in terms of AbiExporterConfigEntry

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,28 @@
-export interface AbiExporterUserConfigEntry {
-  path?: string;
-  runOnCompile?: boolean;
-  clear?: boolean;
-  flat?: boolean;
-  tsWrapper?: boolean;
-  only?: string[];
-  except?: string[];
-  spacing?: number;
-  pretty?: boolean;
-  format?: 'minimal' | 'full' | 'json' | 'typescript';
-  filter?: (
+export interface AbiExporterConfigEntry {
+  path: string;
+  runOnCompile: boolean;
+  clear: boolean;
+  flat: boolean;
+  tsWrapper: boolean;
+  only: string[];
+  except: string[];
+  spacing: number;
+  pretty: boolean;
+  format: 'minimal' | 'full' | 'json' | 'typescript';
+  filter: (
     abiElement: any,
     index: number,
     abi: any,
     sourceName: string,
     contractName: string,
   ) => boolean;
-  rename?: (sourceName: string, contractName: string) => string;
+  rename: (sourceName: string, contractName: string) => string;
 }
 
-export type AbiExporterConfigEntry = Required<AbiExporterUserConfigEntry>;
+export type AbiExporterUserConfigEntry = Partial<AbiExporterConfigEntry>;
+
+export type AbiExporterConfig = AbiExporterConfigEntry[];
 
 export type AbiExporterUserConfig =
   | AbiExporterUserConfigEntry
   | AbiExporterUserConfigEntry[];
-
-export type AbiExporterConfig = AbiExporterConfigEntry[];
