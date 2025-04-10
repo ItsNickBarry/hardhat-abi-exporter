a83095f6f8b7d628878ceee44baad731d6afb194
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-10 00:13:42 -0600

    name unused argument

--- src/actions/clear_abi.ts
+++ src/actions/clear_abi.ts
@@ -1,7 +1,7 @@
 import { clearAbi } from '../lib/clear_abi.js';
 import type { NewTaskActionFunction } from 'hardhat/types/tasks';
 
-const action: NewTaskActionFunction = async (_, hre) => {
+const action: NewTaskActionFunction = async (args, hre) => {
   await clearAbi(hre, hre.config.abiExporter);
 };
 
