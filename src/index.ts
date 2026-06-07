25a91885ff79d9a63d5273cdb106d5aa1a1129c8
Nick Barry <itsnickbarry@protonmail.ch>
2026-06-07 16:21:45 +0900

    wrap plugin export with hardhat definePlugin

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import taskAbiClean from './tasks/abi_clean.js';
 import taskAbiExport from './tasks/abi_export.js';
 import './type_extensions.js';
 import { globalOption } from 'hardhat/config';
+import { definePlugin } from 'hardhat/plugins';
 import { ArgumentType } from 'hardhat/types/arguments';
 import type { HardhatPlugin } from 'hardhat/types/plugins';
 
-const plugin: HardhatPlugin = {
+const plugin: HardhatPlugin = definePlugin({
   id: pkg.name!,
   npmPackage: pkg.name!,
   dependencies: () => [import('@solidstate/hardhat-solidstate-utils')],
@@ -25,6 +26,6 @@ const plugin: HardhatPlugin = {
       type: ArgumentType.BOOLEAN,
     }),
   ],
-};
+});
 
 export default plugin;
